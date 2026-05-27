bb53d131ffef43cab594927229d07430bfcb594e
Yulia Zavodskova <uliazavodskova@gmail.com>
2026-05-27 11:43:12 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagementApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=taskmanagement.db"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+app.Run(); 
